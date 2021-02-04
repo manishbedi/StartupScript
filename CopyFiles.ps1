8516446c300abfb0450946e748db0792860fe2df
manishbedi <53734783+manishbedi@users.noreply.github.com>
2021-02-04 07:13:01 +0530

    Add files via upload

---
+++ CopyFiles.ps1
@@ -0,0 +1,7 @@
+﻿$LocalAppDataLocation = $env:LOCALAPPDATA + “\Packages\NotepadPlusPlus_67p03mhbwwxar\LocalCache\Roaming”
+
+Copy-Item -Path “C:\Program Files\WindowsApps\NotepadPlusPlus_7.8.8.0_x64__67p03mhbwwxar\VFS\AppData\*.*” -Destination $LocalAppDataLocation -Recurse -Force
+
+
+#Copy-Item -Path C:\Temp\T1  -Destination C:\T3 -Recurse -Force
+#Copy-Item -Path C:\Temp\*.* -Destination C:\T3 -Recurse -Force
